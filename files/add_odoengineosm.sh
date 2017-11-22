578c429909573ed3d763d6c935a7801b0428b491
Maaz Anjum <maazanjum@gmail.com>
2017-11-22 14:11:27 -0600

    entity exists checks

--- files/add_odoengineosm.sh
+++ files/add_odoengineosm.sh
@@ -4,46 +4,54 @@ entityname=$(hostname -s)_osm
 output=$basedir/odoengineosm.json
 source $basedir/odomeIds
 
-echo "{"                                                                                > $output
-echo "    \"entityType\": \"usr_odo_engine_osm\","  >> $output
-echo "    \"entityName\": \"$entityname\","     >> $output
-echo "    \"displayName\": \"OSM\","     >> $output
-echo "    \"properties\": {"                                    >> $output
-echo "    \"capability\": {"                                    >> $output
-echo "    \"displayName\": \"Capability\","     >> $output
-echo "    \"value\": \"monitoring\""                    >> $output
-echo "    },"                                                                   >> $output
-echo "    \"orcl_usr_gtp1\": {" >> $output
-echo "    \"displayName\" : \"Campaign\"," >> $output
-echo "    \"value\": \"$odocampaign_name\"" >> $output
-echo "    }" >> $output
-echo "    },"                                                                   >> $output
-echo "    \"tags\":{" >> $output
-echo "    \"campaign\" : \"$odocampaign_name\"," >> $output
-echo "    \"campaign\" : \"$odoengine_name\"" >> $output
-echo "    },"                                                                   >> $output
-echo "    \"availabilityStatus\": \"UP\","              >> $output
-echo "    \"meClass\": \"TARGET\","                     >> $output
-echo "    \"agentBasedAvailability\": \"UP\""   >> $output
-echo "}"                                                                                >> $output
+checkentity="python entity_check.py $entityname usr_odo_engine_osm"
+result=$(eval $checkentity)
+
+if [ $result != "0" ]; then
+        meId=$result
+        meName=$entityname
+else
+        echo "{"                                                                                > $output
+        echo "    \"entityType\": \"usr_odo_engine_osm\","  >> $output
+        echo "    \"entityName\": \"$entityname\","     >> $output
+        echo "    \"displayName\": \"OSM\","     >> $output
+        echo "    \"properties\": {"                                    >> $output
+        echo "    \"capability\": {"                                    >> $output
+        echo "    \"displayName\": \"Capability\","     >> $output
+        echo "    \"value\": \"monitoring\""                    >> $output
+        echo "    },"                                                                   >> $output
+        echo "    \"orcl_usr_gtp1\": {" >> $output
+        echo "    \"displayName\" : \"Campaign\"," >> $output
+        echo "    \"value\": \"$odocampaign_name\"" >> $output
+        echo "    }" >> $output
+        echo "    },"                                                                   >> $output
+        echo "    \"tags\":{" >> $output
+        echo "    \"campaign\" : \"$odocampaign_name\"," >> $output
+        echo "    \"campaign\" : \"$odoengine_name\"" >> $output
+        echo "    },"                                                                   >> $output
+        echo "    \"availabilityStatus\": \"UP\","              >> $output
+        echo "    \"meClass\": \"TARGET\","                     >> $output
+        echo "    \"agentBasedAvailability\": \"UP\""   >> $output
+        echo "}"                                                                                >> $output
 
 
-meId=$(curl -X POST \
-  https://uscgbuodotrial.itom.management.us2.oraclecloud.com/serviceapi/tm-data/mes \
-  -H 'authorization: Basic dXNjZ2J1b2RvdHJpYWwubWFhei5hbmp1bUBvcmFjbGUuY29tOlRlc3QhMjM0' \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'postman-token: b551b292-16a5-5e31-5c96-db63acbfcbbf' \
-  -d '@/home/oracle/scripts/odo/odoengineosm.json' \
-        | grep -o -P '(?<=\"meId\":\").*(?=\")')
+        meId=$(curl -X POST \
+                https://uscgbuodotrial.itom.management.us2.oraclecloud.com/serviceapi/tm-data/mes \
+                -H 'authorization: Basic dXNjZ2J1b2RvdHJpYWwubWFhei5hbmp1bUBvcmFjbGUuY29tOlRlc3QhMjM0' \
+                -H 'cache-control: no-cache' \
+                -H 'content-type: application/json' \
+                -H 'postman-token: b551b292-16a5-5e31-5c96-db63acbfcbbf' \
+                -d '@/home/oracle/scripts/odo/odoengineosm.json' \
+                | grep -o -P '(?<=\"meId\":\").*(?=\")')
 
-meName=$(curl -X GET \
-  https://uscgbuodotrial.itom.management.us2.oraclecloud.com/serviceapi/entityModel/data/entities/$meId \
-  -H 'authorization: Basic dXNjZ2J1b2RvdHJpYWwubWFhei5hbmp1bUBvcmFjbGUuY29tOlRlc3QhMjM0' \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'postman-token: 054f36fd-aee3-b8ed-d04c-c334f8615601' \
-        |  grep -o -P '(?<=\"entityName\":\").*(?=\",\"properties)')
+        meName=$(curl -X GET \
+                https://uscgbuodotrial.itom.management.us2.oraclecloud.com/serviceapi/entityModel/data/entities/$meId \
+                -H 'authorization: Basic dXNjZ2J1b2RvdHJpYWwubWFhei5hbmp1bUBvcmFjbGUuY29tOlRlc3QhMjM0' \
+                -H 'cache-control: no-cache' \
+                -H 'content-type: application/json' \
+                -H 'postman-token: 054f36fd-aee3-b8ed-d04c-c334f8615601' \
+                |  grep -o -P '(?<=\"entityName\":\").*(?=\",\"properties)')
+fi
 
 # Write variable to file - to be used later
 odomeIds=$basedir/odomeIds
@@ -55,3 +63,4 @@ else
         echo "odoengineosm_meId"=$meId >> $odomeIds
         echo "odoengineosm_name"=$meName >> $odomeIds
 fi
+
---
+++ files/entity_check.py
@@ -0,0 +1,25 @@
+import requests, json, datetime, sys
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError
+
+thisentityId = 0
+get_base_entityurl = "https://uscgbuodotrial.itom.management.us2.oraclecloud.com/serviceapi/entityModel/data/entities/?entityName="
+
+headers = {
+    'content-type': "application/json",
+    'authorization': "Basic dXNjZ2J1b2RvdHJpYWwubWFhei5hbmp1bUBvcmFjbGUuY29tOlRlc3QhMjM0",
+    'cache-control': "no-cache",
+    'postman-token': "d6f9b060-5326-24fd-a711-a2373ea4396f"
+    }
+
+get_entityurl = get_base_entityurl + sys.argv[1] + '&entityType=' + sys.argv[2]
+
+r = requests.get(get_entityurl, headers=headers)
+entity = r.json()
+
+for entityentry in entity['items']:
+	thisentityId = entityentry['entityId']
+
+if thisentityId == "":
+	print 0
+else:
+	print 1
