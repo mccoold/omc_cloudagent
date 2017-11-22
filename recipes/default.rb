bf78d8ed8bbab3f1120cda3b3eedc4059e5b8517
Daryn McCool <mdaryn@hotmail.com>
2017-11-22 20:18:05 +0000

    added a new file to pull down

--- recipes/default.rb
+++ recipes/default.rb
@@ -49,7 +49,7 @@ end
   omc_script_list  = ['add_odocampaign.sh', 'add_associations.sh', 'kill.sh', 'add_odoengineasap.sh', 'add_odoenginedbinstance.sh', 
 'add_odoengineosm.sh', 'add_odoengine.sh', 'collect_metrics.sh', 'collect_status.sh', 
 'create_odoentities.sh',  'odoengine_capacity.sh',  'prereq_scripts.sh', 'odoengine_capacity_orderids.sql', 
-'odoengine_capacity_tablespace.sql', 'odoengine_metric_ordersummary.sql', 'taint.sh', 
+'odoengine_capacity_tablespace.sql', 'odoengine_metric_ordersummary.sql', 'taint.sh', 'enity_check.sh', 
 'odoengine_metric_partition_count.sql', 'prereq_db_directory.sql', 'monitor_engine.sh', 'get_engine_name.py']
 
   omc_script_list.each  do |omcscripts| 
