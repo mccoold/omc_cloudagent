1fad1736d3cd5649819e20a39911e36068583b62
Maaz Anjum <maazanjum@gmail.com>
2017-11-28 18:08:03 -0600

    entity create script re-odrer

--- files/create_odoentities.sh
+++ files/create_odoentities.sh
@@ -2,10 +2,10 @@
 basedir=/home/oracle/scripts/odo
 
 $basedir/add_odocampaign.sh
+$basedir/add_odoengine.sh
 $basedir/add_odoengineasap.sh
 $basedir/add_odoenginedbinstance.sh
 $basedir/add_odoengineosm.sh
-$basedir/add_odoengine.sh
 $basedir/add_associations.sh
 
 #touch success script for chef
