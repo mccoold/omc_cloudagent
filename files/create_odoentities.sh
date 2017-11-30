0d3e08b60df64536b69e134b7ab68342b6adf976
Maaz Anjum <maazanjum@gmail.com>
2017-11-30 08:58:03 -0600

    script fixes

--- files/create_odoentities.sh
+++ files/create_odoentities.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 basedir=/home/oracle/scripts/odo
-source $basedir/odomeIds
 
 $basedir/add_odocampaign.sh
 $basedir/add_odoengine.sh
@@ -8,6 +7,9 @@ $basedir/add_odoengineasap.sh
 $basedir/add_odoenginedbinstance.sh
 $basedir/add_odoengineosm.sh
 $basedir/add_associations.sh
+
+
+source $basedir/odomeIds
 
 # Create ODO Engine's Group
 curl -X POST \
