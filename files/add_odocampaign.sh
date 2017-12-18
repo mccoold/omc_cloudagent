444d5f98932bc2926dd7ac89b61b2d82c51a0e43
Maaz Anjum <maazanjum@gmail.com>
2017-12-17 21:23:42 -0600

    engine incarnation changes

--- files/add_odocampaign.sh
+++ files/add_odocampaign.sh
@@ -43,24 +43,37 @@ if [ "$enginenumber" -lt "1" ]; then
           -H 'content-type: application/json' \
                 |  grep -o -P '(?<=\"entityName\":\").*(?=\",\"properties)')
 
+	if [ -z "$meName" ]
+	then
+		meName=$entityname
+	fi
+
+	# This code will determine which incarnation to use for the engine name
+	# <hostname -s>.<incarnation>_engine
+	# for example; jarvis-0.0_engine
+	pythoncommand="python get_engine_name.py"
+	engineincarnation=$(eval $pythoncommand)
+
 	output=$basedir/odocampaign_createdate.json
 
-        echo "{ \"entityName\": \"$entityname\", " > $output
-        echo "  \"entityId\": \"$meId\", " >> $output
-        echo "  \"collectionTs\": \""$mydate"\", " >> $output
-        echo "  \"entityType\": \"usr_odo_campaign\", " >> $output
-        echo "  \"metricGroup\" : \"campaign_inception\", " >> $output
-        echo "  \"metricNames\" : [ " >> $output
-        echo "    \"create_date\" ], " >> $output
-        echo "  \"metricValues\" : [[ " >> $output
-        echo "    \"$(date)\"]]} " >> $output
+	if [[ $engineincarnation -eq 0 ]]; then
+		echo "{ \"entityName\": \"$entityname\", " > $output
+		echo "  \"entityId\": \"$meId\", " >> $output
+		echo "  \"collectionTs\": \""$mydate"\", " >> $output
+		echo "  \"entityType\": \"usr_odo_campaign\", " >> $output
+		echo "  \"metricGroup\" : \"campaign_inception\", " >> $output
+		echo "  \"metricNames\" : [ " >> $output
+		echo "    \"create_date\" ], " >> $output
+		echo "  \"metricValues\" : [[ " >> $output
+		echo "    \"$(date)\"]]} " >> $output
 
-	curl -X POST \
-  		https://uscgbuodo2trial.itom.management.us2.oraclecloud.com/serviceapi/entityModel/data/metrics/ \
-  		-H 'authorization: Basic dXNjZ2J1b2RvMnRyaWFsLm1hYXouYW5qdW1Ab3JhY2xlLmNvbTpUZXN0ITIzNA==' \
-  		-H 'cache-control: no-cache' \
-  		-H 'content-type: application/octet-stream' \
-  		-d '@/home/oracle/scripts/odo/odocampaign_createdate.json'	
+		curl -X POST \
+			https://uscgbuodo2trial.itom.management.us2.oraclecloud.com/serviceapi/entityModel/data/metrics/ \
+			-H 'authorization: Basic dXNjZ2J1b2RvMnRyaWFsLm1hYXouYW5qdW1Ab3JhY2xlLmNvbTpUZXN0ITIzNA==' \
+			-H 'cache-control: no-cache' \
+			-H 'content-type: application/octet-stream' \
+			-d '@/home/oracle/scripts/odo/odocampaign_createdate.json'	
+	fi
 
         # Write variable to file - to be used later
         odomeIds=$basedir/odomeIds
